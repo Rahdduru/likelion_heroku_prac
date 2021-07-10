1ab38c21295375d74e4c0c95e3d6838496904ed3
dahye <smallgilr@gmail.com>
2021-07-10 21:41:44 +0900

    2021-07-10-4

--- heroku/settings.py
+++ heroku/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'herokuApp.apps.HerokuappConfig',
+    'herokuApp',
 ]
 
 MIDDLEWARE = [
